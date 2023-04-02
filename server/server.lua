a70b3a989a5936f3a4fed204389277024582b59b
FuriousAnticheat <124432035+lucidot@users.noreply.github.com>
2023-04-02 15:23:47 -0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,6 +1,5 @@
 
 RegisterServerEvent('pausemenu:quit')
 AddEventHandler('pausemenu:quit', function()
-DropPlayer(source,"NovaPause: You Have Dissconnected Through The Pause Menu\n 👋: Feel Free To Join Back :)")
-
+ForceSocialClubUpdate()
 end)
